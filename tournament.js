800fa64910254fc7df6d44a48a5952a326f0c6c3
AlejandroVillarreal <alexvodark@hotmail.com>
2024-12-04 11:16:26 -0600

    generateRound wip

--- tournament.js
+++ tournament.js
@@ -1,7 +1,56 @@
 // For the theme switcher / unneeded JS
 
 $(document).ready(function () {
-  console.log("Working!");
+
+  let teamsArray = [
+    { id: 18, name: "SKT" },
+    { id: 7, name: "RDX" },
+    { id: 35, name: "LYN" },
+    { id: 2, name: "BLG" },
+    { id: 25, name: "FNC" },
+    { id: 12, name: "PNG" },
+    { id: 41, name: "C9" },
+    { id: 8, name: "G2" },
+    { id: 29, name: "GEN" },
+    { id: 3, name: "DRX" },
+    { id: 22, name: "RNG" },
+    { id: 39, name: "EDG" },
+    { id: 15, name: "TES" },
+    { id: 11, name: "JD" },
+    { id: 33, name: "DK" },
+    { id: 5, name: "MAD" },
+    { id: 26, name: "100T" },
+    { id: 37, name: "TL" },
+    { id: 21, name: "EG" },
+    { id: 17, name: "BBL" },
+    { id: 42, name: "VIT" },
+    { id: 10, name: "KDF" },
+    { id: 4, name: "HLE" },
+    { id: 23, name: "NS" },
+    { id: 44, name: "KT" },
+    { id: 1, name: "PSG" },
+    { id: 13, name: "FLY" },
+    { id: 9, name: "TSM" },
+    { id: 38, name: "IMT" },
+    { id: 6, name: "BYG" },
+    { id: 28, name: "R7" },
+    { id: 24, name: "DFM" },
+    { id: 14, name: "LOUD" },
+    { id: 20, name: "ISG" },
+    { id: 32, name: "SBG" },
+    { id: 19, name: "V3" },
+    { id: 31, name: "CFO" },
+    { id: 43, name: "APL" },
+    { id: 16, name: "ORG" },
+    { id: 34, name: "VCT" },
+  ];
+  var dropdown = $('#teamCount');
+  dropdown.append(new Option(2));
+  dropdown.append(new Option(4));
+  dropdown.append(new Option(8));
+  dropdown.append(new Option(16));
+  dropdown.append(new Option(32));
+  
   $("#theme-none").click(function (e) {
     $(".theme").removeClass("theme-light");
     $(".theme").removeClass("theme-dark");
@@ -9,12 +58,22 @@ $(document).ready(function () {
   });
 
   $("#btnGenerateMatch").click(function () {
-    GenerateMatch(1);
+    var numberOfTeams = $('#teamCount').val();
+    let teams = shuffleArray(teamsArray);
+    try {
+        teams = teams.slice(0,Number(numberOfTeams));
+        GenerateMatch(1,teams);
+    } catch(error) {
+        alert("Not enough teams to generate the tournament");
+    }
+    
   });
   $("#btnGenerateMatch2").click(function () {
     GenerateMatch2(2);
   });
-  function GenerateMatch(roundNumber) {
+  
+//#region Functions
+function GenerateMatch(roundNumber, teams) {
     const tournamentContainer = $("#tournament-structure");
     var roundContainer = $("<div>").addClass("column one");
     if (roundNumber > 1) {
@@ -22,7 +81,7 @@ $(document).ready(function () {
       roundContainer.addClass("two");
     }
 
-    for (let i = 0; i < 32; i += 2) {
+    for (let i = 0; i < teams.length; i += 2) {
       const matchWinner = $("<div>").addClass("match winner-top");
 
       matchWinner.html(`
@@ -48,7 +107,6 @@ $(document).ready(function () {
 
     tournamentContainer.append(roundContainer);
   }
-
   function GenerateMatch2(roundNumber) {
     //This generates the second match reducing the team number by half and takes round
     //number as 2 changing the column class to two
@@ -81,7 +139,7 @@ $(document).ready(function () {
                     </div>
                 
                 `);
-    altLine.html(`
+      altLine.html(`
                 <div class= "line one"></div>
                     `);
       matchWinner.append(altLine);
@@ -94,4 +152,13 @@ $(document).ready(function () {
   function isOdd(num) {
     return num % 2 !== 0;
   }
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    }
+    return array;
+  }
+  //#endregion
 });
